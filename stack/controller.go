46b5147dcf295f046de40543b913c9648b5c7224
Amit Bhadoria <gnulib@gmail.com>
2018-10-04 19:59:32 -0700

    Issue #22: Added DLT stack controller interface Added interface to register, unregister and submit transaction

--- stack/controller.go
+++ stack/controller.go
@@ -4,17 +4,100 @@ package stack
 
 import (
 	"errors"
+	"sync"
 	"github.com/trust-net/dag-lib-go/db"
 )
 
+type AppConfig struct {
+	// public ID of the application node
+	NodeId []byte
+	// name of the application node
+	NodeName string
+	// shard ID of the application (same for all nodes of application)
+	ShardId []byte
+	// protocol version for the shard (same for all nodes of application)
+	Version uint64
+}
+
+type Transaction struct {
+	// serialized transaction payload
+	Payload []byte
+	// transaction signature
+	Signature []byte
+	// transaction approver node's public ID
+	NodeId []byte
+	// transaction submitter's public ID
+	Submitter []byte
+}
+
+// approve if connection request is from a valid application peer
+type PeerApprover func (app AppConfig) bool
+
+// approve if a recieved network transaction is valid 
+type NetworkTxApprover func (tx *Transaction) error
+
 type DLT interface {
-	// TBD
+	// register application shard with the DLT stack
+	Register(app AppConfig, peerHandler PeerApprover, txHandler NetworkTxApprover) error
+	// unregister application shard from DLT stack
+	Unregister() error
+	// submit a transaction to the network
+	Submit(tx *Transaction) error
 }
 
 type dlt struct {
-	// TBD
+	app *AppConfig
+	peerHandler PeerApprover
+	txHandler NetworkTxApprover
+	db db.Database
+	lock   sync.RWMutex
+}
+
+func (d *dlt) Register(app AppConfig, peerHandler PeerApprover, txHandler NetworkTxApprover) error {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	if d.app != nil {
+		return errors.New("App is already registered")
+	}
+	d.app = &app
+	d.peerHandler = peerHandler
+	d.txHandler = txHandler
+	return nil
+}
+
+func (d *dlt) Unregister() error {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	d.app = nil
+	d.peerHandler = nil
+	d.txHandler = nil
+	return nil
+}
+
+func (d *dlt) Submit(tx *Transaction) error {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	if d.app == nil {
+		return errors.New("app not registered")
+	}
+	if tx == nil {
+		return errors.New("nil transaction")
+	}
+	switch {
+		case tx.Payload == nil:
+			return errors.New("nil transaction payload")
+		case tx.Signature == nil:
+			return errors.New("nil transaction signature")
+		case tx.NodeId == nil:
+			return errors.New("nil transaction node ID")
+		case tx.Submitter == nil:
+			return errors.New("nil transaction submitter ID")
+	}
+	return nil
 }
 
 func NewDltStack(db db.Database) (*dlt, error) {
-	return nil, errors.New("not yet implemented")
+	return &dlt {
+		db: db,
+	}, nil
 }
--- stack/controller_test.go
+++ stack/controller_test.go
@@ -8,8 +8,128 @@ import (
 func TestInitiatization(t *testing.T) {
 	var stack DLT
 	var err error
-	stack, err = NewDltStack(db.NewInMemDatabase())
-	if stack.(*dlt) != nil || err == nil {
+	testDb := db.NewInMemDatabase()
+	stack, err = NewDltStack(testDb)
+	if stack.(*dlt) == nil || err != nil {
 		t.Errorf("Initiatization validation failed, c: %s, err: %s", stack, err)
+	}
+	if stack.(*dlt).db != testDb {
+		t.Errorf("Stack does not have correct DB reference expected: %s, actual: %s", testDb, stack.(*dlt).db)
+	}
+}
+
+func TestRegister(t *testing.T) {
+	stack, _ := NewDltStack(db.NewInMemDatabase())
+	app := AppConfig{}
+	peerHandler := func (app AppConfig) bool {return true}
+	txHandler := func (tx *Transaction) error {return nil}
+	
+	if err := stack.Register(app, peerHandler, txHandler); err != nil {
+		t.Errorf("Registration failed, err: %s", err)
+	}
+	if string(stack.app.NodeId) != string(app.NodeId) || string(stack.app.ShardId) != string(app.ShardId) || stack.app.NodeName != app.NodeName {
+		t.Errorf("App configuration not initialized correctly")
+	}
+	if stack.peerHandler == nil || stack.txHandler == nil {
+		t.Errorf("Callback methods not initialized correctly")
+	}
+}
+
+func TestPreRegistered(t *testing.T) {
+	stack, _ := NewDltStack(db.NewInMemDatabase())
+	app := AppConfig{}
+	peerHandler := func (app AppConfig) bool {return true}
+	txHandler := func (tx *Transaction) error {return nil}
+	
+	if err := stack.Register(app, peerHandler, txHandler); err != nil {
+		t.Errorf("Registration failed, err: %s", err)
+	}
+	
+	if err := stack.Register(AppConfig{}, peerHandler, txHandler); err == nil {
+		t.Errorf("Registration did not check for already registered")
+	}
+}
+
+func TestUnRegister(t *testing.T) {
+	stack, _ := NewDltStack(db.NewInMemDatabase())
+	peerHandler := func (app AppConfig) bool {return true}
+	txHandler := func (tx *Transaction) error {return nil}
+	
+	if err := stack.Register(AppConfig{}, peerHandler, txHandler); err != nil {
+		t.Errorf("Registration failed, err: %s", err)
+	}
+	if err := stack.Unregister(); err != nil {
+		t.Errorf("Unregistration failed, err: %s", err)
+	}
+	if stack.app != nil {
+		t.Errorf("App configuration not cleared during unregister")
+	}
+	if stack.peerHandler != nil || stack.txHandler != nil {
+		t.Errorf("Callback methods not cleared during unregister")
+	}
+}
+
+func TestSubmitUnregistered(t *testing.T) {
+	stack, _ := NewDltStack(db.NewInMemDatabase())
+	if err := stack.Submit(nil); err == nil {
+		t.Errorf("Transaction submission did not check for unregistered")
+	}
+}
+
+func testTransaction() *Transaction {
+	return &Transaction {
+		Payload: []byte("test data"),
+		Signature: []byte("test signature"),
+		NodeId: []byte("test node ID"),
+		Submitter: []byte("test submitter"),
+	}
+}
+
+func TestSubmitNilValues(t *testing.T) {
+	stack, _ := NewDltStack(db.NewInMemDatabase())
+	app := AppConfig{}
+	peerHandler := func (app AppConfig) bool {return true}
+	txHandler := func (tx *Transaction) error {return nil}	
+	if err := stack.Register(app, peerHandler, txHandler); err != nil {
+		t.Errorf("Registration failed, err: %s", err)
+		return
+	}
+	if err := stack.Submit(nil); err == nil {
+		t.Errorf("Transaction submission did not check for nil transaction")
+	}
+	tx := testTransaction()
+	tx.Payload = nil
+	if err := stack.Submit(tx); err == nil {
+		t.Errorf("Transaction submission did not check for nil payload")
+	}
+	tx = testTransaction()
+	tx.Signature = nil
+	if err := stack.Submit(tx); err == nil {
+		t.Errorf("Transaction submission did not check for nil signature")
+	}
+	tx = testTransaction()
+	tx.NodeId = nil
+	if err := stack.Submit(tx); err == nil {
+		t.Errorf("Transaction submission did not check for nil node ID")
+	}
+	tx = testTransaction()
+	tx.Submitter = nil
+	if err := stack.Submit(tx); err == nil {
+		t.Errorf("Transaction submission did not check for nil submitter ID")
+	}
+}
+
+func TestSubmit(t *testing.T) {
+	stack, _ := NewDltStack(db.NewInMemDatabase())
+	app := AppConfig{}
+	peerHandler := func (app AppConfig) bool {return true}
+	txHandler := func (tx *Transaction) error {return nil}
+	
+	if err := stack.Register(app, peerHandler, txHandler); err != nil {
+		t.Errorf("Registration failed, err: %s", err)
+		return
+	}
+	if err := stack.Submit(testTransaction()); err != nil {
+		t.Errorf("Transaction submission failed, err: %s", err)
 	}
 }
